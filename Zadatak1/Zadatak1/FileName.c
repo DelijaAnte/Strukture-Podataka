b6af57cc8c61a1732ba1b76cf42cd8604237e0c6
DelijaAnte <antedelija77@gmail.com>
2023-11-02 10:16:29 +0100

    .

--- Zadatak1/Zadatak1/FileName.c
+++ Zadatak1/Zadatak1/FileName.c
@@ -1,44 +1,116 @@
 #define _CRT_SECURE_NO_WARNINGS
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-#define MAX_SIZE (50)
-#define MAX_LINE (1024)
-#define FILE_ERROR_OPEN (-1)
+#define MAXNAME 128
+#define MAXRED 1024
+#define MAXBOD 50
 
-typedef struct _student {
-    char name[MAX_SIZE];
-    char surname[MAX_SIZE];
-    double points;
-} Student;
+typedef struct {
+    char ime[MAXNAME];
+    char prezime[MAXNAME];
+    int bodovi;
+}Student;
 
-int brojacRedova(const char* imefilea) {
-    FILE* filePointer = NULL;
-    char buffer[MAX_LINE] = { 0 };
-    int counter = 0;
+int brojacRedova(char imeDatoteke[MAXNAME]) {
+    int brojac = 0;
+    FILE* fp = NULL;
 
-    filePointer = fopen(imefilea, "r");
-
-    if (!filePointer) {
-        perror("Error opening file");
-        return FILE_ERROR_OPEN;
+    fp = fopen(imeDatoteke, "r");
+    if (fp == NULL) {
+        printf("\nGreska u otvaranju datoteke!");
+    }
+    else {
+        printf("\nUspjesno otvaranje datoteke!");
     }
 
-    while (fgets(buffer, MAX_LINE, filePointer) != NULL) {
-        counter++;
+    char buffer[MAXRED] = { 0 };
+
+    while (!feof(fp)) {
+        fgets(buffer, MAXRED, fp);
+        if (strcmp("\n", buffer) != 0) {
+            brojac++;
+        }
     }
 
-    fclose(filePointer);
+    fclose(fp);
+    return brojac;
+}
 
-    return counter;
+void citanjeStud(Student* stud, int brStud, char imeDatoteke[MAXNAME]) {
+    FILE* fp = NULL;
+    int i = 0;
+
+    fp = fopen(imeDatoteke, "r");
+
+    if (fp == NULL) {
+        printf("\nGreska u otvaranju datoteke!");
+    }
+    else {
+        printf("\nUspjesno otvaranje datoteke!");
+    }
+
+    for (i = 0; i < brStud; i++) {
+        fscanf(fp, "%s %s %d\n", (stud + i)->ime, (stud + i)->prezime, &((stud + i)->bodovi));
+    }
+
+    fclose(fp);
+    return;
+}
+
+void ispisStud(Student* stud, int brStud) {
+    double prosjek = 0;
+    int i = 0;
+
+    for (i = 0; i < brStud; i++) {
+        prosjek = (double)(stud + i)->bodovi / MAXBOD;
+        printf("\nStudent %s %s imao je %d bodova, odnosno %.2lf posto (relativni bodovi).", (stud + i)->ime, (stud + i)->prezime, (stud + i)->bodovi, prosjek * 100);
+    }
 }
 
 int main() {
-    int noRows = 0;
 
-    noRows = brojacRedova("studenti.txt");
+    char imeDatoteke[MAXNAME] = { 0 };
 
-    printf("%d\n", noRows);
+    FILE* test = NULL;
+
+    while (test == NULL) {
+        printf("\nMolimo upišite ime datoteke sa listom studenata:\n");
+        scanf("%s", imeDatoteke);
+
+        test = fopen(imeDatoteke, "r");
+
+        if (test == NULL) {
+            printf("\nGreska u otvaranju datoteke!");
+        }
+        else {
+            printf("\nUspjesno otvaranje datoteke!");
+            fclose(test);  // zatvori otvorenu datoteku
+        }
+
+        getchar();
+    }
+
+    int brojStudenata = 0;
+    brojStudenata = brojacRedova(imeDatoteke);
+
+    Student* stud = NULL;
+    stud = (Student*)malloc(brojStudenata * sizeof(Student));
+
+    if (stud == NULL) {
+        printf("Alokacija neuspjesna.\n");
+    }
+    else {
+        printf("Alokacija je uspjesna.\n");
+    }
+
+    citanjeStud(stud, brojStudenata, imeDatoteke);
+
+    ispisStud(stud, brojStudenata);
+
+    free(stud);
 
     return 0;
 }
